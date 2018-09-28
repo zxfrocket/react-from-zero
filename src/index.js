feb9a4c0c47ada00c7cf2eafef34e7471e312a50
Xuefeng Zhou <xzhou@rocketsoftware.com>
2018-09-28 13:50:38 +0800

    form

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import uuid from 'uuid';
+import { timingSafeEqual } from 'crypto';
 
-class ADS_Title extends React.Component{
-  constructor(props){
+class ADS_Title extends React.Component {
+  constructor(props) {
     super(props)
   }
 
-  deleteItem(){
+  deleteItem() {
     this.props.delete(this.props.id);
   }
 
-  static get defaultProps(){
+  static get defaultProps() {
     return {
       name: 'No Country'
     };
@@ -31,8 +32,8 @@ class ADS_Title extends React.Component{
   }
 }
 
-class ADS_Container extends React.Component{
-  constructor(props){
+class ADS_Container extends React.Component {
+  constructor(props) {
     super(props)
     this.state = {
       items: [
@@ -52,7 +53,7 @@ class ADS_Container extends React.Component{
     };
   }
 
-  addOneLine(){
+  addOneLine() {
     this.state.items.push({
       //name: 'UK',
       arr: ['B.Tim', 'G.Andy', 'C.Joe'],
@@ -62,7 +63,7 @@ class ADS_Container extends React.Component{
     this.forceUpdate();
   }
 
-  deleteOneLine(id){
+  deleteOneLine(id) {
     const idx = this.state.items.findIndex(item => item.id === id);
     this.state.items.splice(idx, 1);
     this.forceUpdate();
@@ -84,8 +85,88 @@ class ADS_Container extends React.Component{
   }
 }
 
+class ADS_Form extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      surInputValue: 'test value',
+      surRadioValue: 'banana',
+      inputValue: 'test value',
+      surFruits: ['apple', 'grape']
+    }
+  }
+
+  changeSurInput(event){
+    this.state.surInputValue = event.target.value
+  }
+
+  changeInput(event){
+    this.setState({
+      inputValue: event.target.value
+    });
+  }
+
+  surChangeRadioValue(event){
+    this.state.surRadioValue = event.target.value
+    console.log(`surChangeRadioValue is ${this.state.surRadioValue}`)
+  }
+
+  surChangeMultiValue(event){
+    this.state.surFruits = event.target.value
+    console.log(`surFruits is ${this.state.surFruits}`)
+  }
+
+  componentDidMount(){
+    this.refs.supInput.focus()
+  }
+
+  render() {
+    return <div>
+      <form>
+        <input type="text" defaultValue={this.state.surInputValue} onChange={this.changeSurInput.bind(this)}></input>
+        <input type="checkbox" defaultChecked="true" value="fruits"></input>
+        <input type="radio" name="blend" defaultChecked="true" value="apple"></input>
+        <input type="radio" name="blend" value="banana"></input>
+        <select defaultValue={this.state.surRadioValue} onChange={this.surChangeRadioValue.bind(this)}>
+          <option value="apple">apple</option>
+          <option value="orange">orange</option>
+          <option value="banana">banana</option>
+          <option value="grape">grape</option>
+        </select>
+        <select multiple defaultValue={this.state.surFruits} onChange={this.surChangeMultiValue.bind(this)}>
+          <option value="apple">apple</option>
+          <option value="orange">orange</option>
+          <option value="banana">banana</option>
+          <option value="grape">grape</option>
+        </select>
+      </form>
+      <form>
+        <input type="text"  ref="supInput" value={this.state.inputValue} onChange={this.changeInput.bind(this)}></input>
+        <input type="checkbox" defaultChecked="true" value="fruits"></input>
+        <input type="radio" name="blend" defaultChecked="true" value="apple"></input>
+        <input type="radio" name="blend" value="banana"></input>
+        <select defaultValue="banana">
+          <option value="apple">apple</option>
+          <option value="orange">orange</option>
+          <option value="banana">banana</option>
+          <option value="grape">grape</option>
+        </select>
+        <select multiple>
+          <option value="apple">apple</option>
+          <option value="orange" selected>orange</option>
+          <option value="banana">banana</option>
+          <option value="grape" selected>grape</option>
+        </select>
+      </form>
+    </div>;
+  }
+}
+
 ReactDOM.render(
-  <ADS_Container />,
+  <div>
+    <ADS_Container />
+    <ADS_Form />
+  </div>,
   document.querySelector('#root')
 );
 
