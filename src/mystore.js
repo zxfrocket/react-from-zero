4500c72e3ffedc6875475b5fa3dc51ab7b9d11ee
Xuefeng Zhou <xzhou@rocketsoftware.com>
2018-11-27 17:44:08 +0800

    middleware

--- src/mystore.js
+++ src/mystore.js
@@ -44,8 +44,16 @@ class Store {
   }
 }
 
-const store = new Store({ name: 'leo', num: 5 });
+function createStore(updaters, defaultState) {
+  const store = new Store(defaultState);
+  store.setUpdates(updaters);
+  return store;
+}
 
+const store = createStore({
+  name: nameUpdater,
+  num: numberUpdater
+}, { name: 'leo', num: 5 });
 
 function numberUpdater(oldNum, action) {
   let newNum = oldNum;
@@ -60,20 +68,16 @@ function numberUpdater(oldNum, action) {
   return newNum;
 }
 
-function nameUpdater(oldName, action){
-  if(action.type === 'changeName'){
+function nameUpdater(oldName, action) {
+  if (action.type === 'changeName') {
     return action.name;
   }
   return oldName;
 }
 
-store.setUpdates({
-  name: nameUpdater,
-  num: numberUpdater
-});
 
 store.listen(() => {
-  console.log(store.state)
+  console.log('store listen', store.state)
 })
 
 const action = {
@@ -89,10 +93,60 @@ const action3 = {
   name: 'zhangsan'
 }
 
-store.dispatch(action)
-store.dispatch(action2)
-store.dispatch(action3)
+// console.log('begin', action3.type)
+// store.dispatch(action)
+// store.dispatch(action2)
+// store.dispatch(action3)
+// console.log('end', action3.type)
 
 //store output is store.listen
 //store input is store.setUpdates
 //store trigger is store.dispatch
+
+function logger(store) {
+  let next = store.dispatch;
+
+  store.dispatch = (action) => {
+    console.log('begin logger', action)
+    action.name = 'logger';
+    next.call(store, action) //next here is ajaxData function
+    console.log('end logger', action)
+  };
+
+  return store;
+}
+
+//async
+function ajaxData(store) {
+  let next = store.dispatch;
+
+  store.dispatch = (action) => {
+    if (action.url) {
+      setTimeout(() => {
+        console.log('begin ajaxData', action)
+        next.call(store, action) // next here is store's listen function
+        console.log('end ajaxData', action)
+      }, 1000)
+    } else {
+      next.call(store, action)
+      console.log('never run')
+    }
+  };
+
+  return store;
+
+}
+
+function useMiddleware(store, middles) {
+  middles.reverse();
+  middles.forEach((middle) => {
+    middle(store);
+  });
+  return store;
+}
+
+useMiddleware(store, [logger, ajaxData]);
+
+store.dispatch({type: 'changeName', url: '////'})
+
+//middleware means fetch the store and action then change them before run store.listener, 
